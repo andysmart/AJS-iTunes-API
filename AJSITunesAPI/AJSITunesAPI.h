5c79f19e7d1b955a80398a3d917b3e1532ecc5ca
Andy Smart <andy@andysmart.org>
2013-09-06 15:45:02 +0100

    Added Podfile and linked libs

--- AJSITunesAPI/AJSITunesAPI.h
+++ AJSITunesAPI/AJSITunesAPI.h
@@ -6,8 +6,3 @@
 //  Copyright (c) 2013 Rocket Town Ltd. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface AJSITunesAPI : NSObject
-
-@end
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+
+pod 'AFNetworking', '~> 2.0.0-RC2'
+pod 'ISO8601DateFormatter', '~> 0.6'
+
+inhibit_all_warnings!
